f0a4744188fa983bdfcb36984c47a32301de2cc1
Giedrius Grabauskas <43740166+Grabauskas@users.noreply.github.com>
2025-06-12 13:16:33 +0300

    Added Possibility to Configure Forwarded Headers Trusts (#20)

--- src/OneGround.ZGW.Common.Web/Extensions/ServiceCollection/ZGWApiServiceCollectionExtensions.cs
+++ src/OneGround.ZGW.Common.Web/Extensions/ServiceCollection/ZGWApiServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using Asp.Versioning;
@@ -59,6 +60,38 @@ public static class ZGWApiServiceCollectionExtensions
         services.Configure<ForwardedHeadersOptions>(options =>
         {
             options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+
+            var knownNetworks = configuration.GetSection("ForwardedHeaders:KnownNetworks").Get<string[]>() ?? [];
+            if (knownNetworks.Length != 0)
+            {
+                foreach (var network in knownNetworks)
+                {
+                    var parts = network.Split('/');
+
+                    if (parts.Length == 2 && IPAddress.TryParse(parts[0], out var ipAddress) && int.TryParse(parts[1], out var prefixLength))
+                    {
+                        options.KnownNetworks.Add(new Microsoft.AspNetCore.HttpOverrides.IPNetwork(ipAddress, prefixLength));
+                    }
+                    else
+                    {
+                        throw new FormatException(
+                            $"Invalid network format in 'ForwardedHeaders:KnownNetworks': '{network}'. Expected format is '[IPAddress]/[PrefixLength]'."
+                        );
+                    }
+                }
+            }
+
+            var knownProxies = configuration.GetSection("ForwardedHeaders:KnownProxies").Get<string[]>() ?? [];
+            if (knownProxies.Length != 0)
+            {
+                foreach (var proxy in knownProxies)
+                {
+                    options.KnownProxies.Add(IPAddress.Parse(proxy));
+                }
+            }
 
             bool resolverForwardedHeader = configuration.GetValue("Application:ResolveForwardedHost", false);
             if (resolverForwardedHeader)
