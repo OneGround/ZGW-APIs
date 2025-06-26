16f189c7c20e8004e815ef4a126d5fbd9bfc70c7
Giedrius Grabauskas <43740166+Grabauskas@users.noreply.github.com>
2025-06-26 10:52:31 +0300

    Removed test job trigger

--- src/OneGround.ZGW.Notificaties.Messaging/Jobs/Extensions/NotificatiesJobsServiceExtensions.cs
+++ src/OneGround.ZGW.Notificaties.Messaging/Jobs/Extensions/NotificatiesJobsServiceExtensions.cs
@@ -15,19 +15,5 @@ public static class NotificatiesJobsServiceExtensions
     public static void AddNotificatiesJobsAgent(this IServiceCollection services)
     {
         services.AddTransient<NotificatieJob>();
-
-        Task.Run(async () =>
-        {
-            await Task.Delay(10_000);
-
-            var job = BackgroundJob.Schedule<NotificatieJob>(
-                h =>
-                    h.ReQueueNotificatieAsync(
-                        new SubscriberNotificatie() { Rsin = "000000000", ChannelUrl = "http://example.com" },
-                        TimeSpan.FromMinutes(1)
-                    ),
-                TimeSpan.FromMinutes(1)
-            );
-        });
     }
 }
