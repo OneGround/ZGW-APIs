8ea59bc71d429df1ad4f065072c8067707b25ee5
Paulius Leleika <paulius.leleika@visma.com>
2025-07-15 15:18:29 +0300

    FUND-1632 Update kiota version, lock keycloak openapi specification version

--- localdev/keycloak/KeycloakClient/kiota-generate.bat
+++ localdev/keycloak/KeycloakClient/kiota-generate.bat
@@ -1,5 +1,3 @@
 @echo off
 echo Regenerating Keycloak client...
-dotnet kiota generate --clean-output -o ./src --openapi https://www.keycloak.org/docs-api/latest/rest-api/openapi.json --language csharp -c KeycloakClient -n Keycloak.Client
-
-echo Keycloak client updated successfully!
+dotnet kiota generate --clean-output -o ./src --openapi https://www.keycloak.org/docs-api/26.3.1/rest-api/openapi.json --language csharp -c KeycloakClient -n Keycloak.Client
