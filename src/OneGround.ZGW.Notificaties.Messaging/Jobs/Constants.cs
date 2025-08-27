fe7d44b8e318dc808b56403ff009f981e32dbd24
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2025-08-27 08:47:44 +0200

    constant class made static

--- src/OneGround.ZGW.Notificaties.Messaging/Jobs/Constants.cs
+++ src/OneGround.ZGW.Notificaties.Messaging/Jobs/Constants.cs
@@ -1,6 +1,6 @@
 namespace OneGround.ZGW.Notificaties.Messaging.Jobs;
 
-internal class Constants
+internal static class Constants
 {
     public const string NrcListenerQueue = "nrc_listener_queue";
     public const string NrcListenerServer = "nrc_listener_server";
