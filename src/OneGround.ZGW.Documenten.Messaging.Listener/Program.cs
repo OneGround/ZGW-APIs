105e457c507883d120f155b8017f0c29b409992c
heuvea <66989902+heuvea@users.noreply.github.com>
2025-08-29 11:58:02 +0200

    FUND-1570 NRC- and DRC-Listeners add health-check (#86)
    * added health-endpoints to nrc- and drc-listeners
    
    * dispose CancellationToken
    
    * code review changes
    
    * code review changes ii
    
    * Haproxy config file fixed and README updated
    
    * README fiel corrected
    
    * .ENV file corrected
    
    * code review changes iii

--- src/OneGround.ZGW.Documenten.Messaging.Listener/Program.cs
+++ src/OneGround.ZGW.Documenten.Messaging.Listener/Program.cs
@@ -1,12 +1,16 @@
-using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using OneGround.ZGW.Common.Configuration;
 using OneGround.ZGW.Common.Constants;
 using OneGround.ZGW.Common.Extensions;
 using OneGround.ZGW.Common.Web.Extensions.ServiceCollection;
 using OneGround.ZGW.Documenten.Messaging;
 
-var builder = Host.CreateApplicationBuilder();
+var builder = WebApplication.CreateBuilder(args);
+
 builder.ConfigureHostDefaults(ServiceRoleName.DRC_LISTENER);
+
+builder.Services.AddHealthChecks();
 
 var serviceConfiguration = new ServiceConfiguration(builder.Configuration);
 serviceConfiguration.ConfigureServices(builder.Services);
@@ -15,4 +19,6 @@ builder.Services.RegisterZgwTokenClient(builder.Configuration, builder.Environme
 
 var app = builder.Build();
 
-app.Run();
+app.MapHealthChecks("/health");
+
+await app.RunAsync();
--- src/OneGround.ZGW.Notificaties.Messaging.Listener/Program.cs
+++ src/OneGround.ZGW.Notificaties.Messaging.Listener/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using OneGround.ZGW.Common.Configuration;
 using OneGround.ZGW.Common.Constants;
 using OneGround.ZGW.Common.Extensions;
 using OneGround.ZGW.Notificaties.Messaging;
 using OneGround.ZGW.Notificaties.Messaging.Consumers;
 
-var builder = Host.CreateApplicationBuilder();
+var builder = WebApplication.CreateBuilder(args);
+
 builder.ConfigureHostDefaults(ServiceRoleName.NRC_LISTENER);
+
+builder.Services.AddHealthChecks();
 
 var serviceConfiguration = new ServiceConfiguration(builder.Configuration);
 serviceConfiguration.ConfigureServices(builder.Services);
@@ -17,4 +20,6 @@ builder.Services.AddZGWSecretManager(builder.Configuration);
 
 var app = builder.Build();
 
-app.Run();
+app.MapHealthChecks("/health");
+
+await app.RunAsync();
