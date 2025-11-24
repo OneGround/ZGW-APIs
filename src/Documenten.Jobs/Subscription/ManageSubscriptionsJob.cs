bf83d0946e40e43c229936961a008f2b4cd48a3a
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2025-11-24 10:35:44 +0100

    finished last part

--- src/Documenten.Jobs/Subscription/ManageSubscriptionsJob.cs
+++ src/Documenten.Jobs/Subscription/ManageSubscriptionsJob.cs
@@ -6,13 +6,11 @@ using OneGround.ZGW.Common.Authentication;
 using OneGround.ZGW.Common.Configuration;
 using OneGround.ZGW.Common.CorrelationId;
 using OneGround.ZGW.Common.Services;
-using OneGround.ZGW.Notificaties.ServiceAgent;
 
 namespace Roxit.ZGW.Documenten.Jobs.Subscription;
 
 public class ManageSubscriptionsJob : SubscriptionJobBase<ManageSubscriptionsJob>
 {
-    private readonly INotificatiesServiceAgent _notificatieServiceAgent;
     private readonly IOptionsMonitor<ZgwServiceAccountConfiguration> _optionsMonitor;
 
     public ManageSubscriptionsJob(
@@ -21,7 +19,6 @@ public class ManageSubscriptionsJob : SubscriptionJobBase<ManageSubscriptionsJob
         IOrganisationContextAccessor organisationContextAccessor,
         IOrganisationContextFactory organisationContextFactory,
         IServiceDiscovery serviceDiscovery,
-        INotificatiesServiceAgent notificatieServiceAgent,
         IOptionsMonitor<ZgwServiceAccountConfiguration> optionsMonitor
     )
         : base(
@@ -32,20 +29,19 @@ public class ManageSubscriptionsJob : SubscriptionJobBase<ManageSubscriptionsJob
             serviceDiscovery: serviceDiscovery
         )
     {
-        _notificatieServiceAgent = notificatieServiceAgent;
         _optionsMonitor = optionsMonitor;
     }
 
     [AutomaticRetry(Attempts = 3, DelaysInSeconds = new[] { 5, 30, 120 }, OnAttemptsExceeded = AttemptsExceededAction.Fail)]
     [Queue(Constants.DrcSubscriptionsQueue)]
-    public async Task ExecuteAsync()
+    public Task ExecuteAsync()
     {
         _logger.LogInformation("{ManageSubscriptionsJob} job started.", nameof(ManageSubscriptionsJob));
 
         var currentServiceAccountCredentials = CurrentServiceAccountCredentials;
         var currentRecurringHangfireTokenRefreshJobs = CurrentRecurringHangfireTokenRefreshJobs;
 
-        // 1. Add mising subscriptions for service accounts that do not have one yet
+        // 1. Add missing subscriptions for service accounts that do not have one yet
         foreach (var serviceaccount in currentServiceAccountCredentials)
         {
             if (!currentRecurringHangfireTokenRefreshJobs.Any(job => job.Id == $"create-or-patch-subscription-{serviceaccount.Rsin}-job"))
@@ -54,40 +50,8 @@ public class ManageSubscriptionsJob : SubscriptionJobBase<ManageSubscriptionsJob
             }
         }
 
-        // 2. Remove subscriptions for service accounts that are no longer configured
-        foreach (var recurringJob in currentRecurringHangfireTokenRefreshJobs)
-        {
-            if (recurringJob.Id.StartsWith("create-or-patch-subscription-"))
-            {
-                var rsin = recurringJob.Id.Replace("create-or-patch-subscription-", "").Replace("-job", "");
-                if (!currentServiceAccountCredentials.Any(c => c.Rsin == rsin))
-                {
-                    RecurringJob.RemoveIfExists(recurringJob.Id);
-                }
-
-                // TODO: Bot TESTED yet
-                _correlationContextAccessor.SetCorrelationId(Guid.NewGuid().ToString());
-                _organisationContextAccessor.OrganisationContext = _organisationContextFactory.Create(rsin);
-
-                var result = await _notificatieServiceAgent.GetAllAbonnementenAsync();
-                if (result.Success)
-                {
-                    var organisationSubscribers = result.Response.Where(a => a.Owner == rsin && IsDocumentListenerSubscription(a.CallbackUrl));
-                    foreach (var subscriber in organisationSubscribers)
-                    {
-                        var deleteResult = await _notificatieServiceAgent.DeleteAbonnementByIdAsync(subscriber.Id);
-                        if (!deleteResult.Success)
-                        {
-                            throw new InvalidOperationException(
-                                $"Failed to delete subscription in NRC API for Rsin {rsin}. Error: {deleteResult.Error.Title}"
-                            );
-                        }
-                    }
-                }
-            }
-        }
-
         _logger.LogInformation("{ManageSubscriptionsJob} job finished.", nameof(ManageSubscriptionsJob));
+        return Task.CompletedTask;
     }
 
     private static List<RecurringJobDto> CurrentRecurringHangfireTokenRefreshJobs => JobStorage.Current.GetConnection().GetRecurringJobs();
