b966efbb9464e3657464e4bf862c4bc0239275ab
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2025-11-24 11:01:20 +0100

    added migration to extend field auth of table abonnementen

---
+++ src/OneGround.ZGW.Notificaties.DataModel/Migrations/20251124095038_exend_abonnementen_auth_field_length.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Notificaties.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class exend_abonnementen_auth_field_length : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "auth",
+                table: "abonnementen",
+                type: "character varying(5000)",
+                maxLength: 5000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(1000)",
+                oldMaxLength: 1000
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "auth",
+                table: "abonnementen",
+                type: "character varying(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5000)",
+                oldMaxLength: 5000
+            );
+        }
+    }
+}
