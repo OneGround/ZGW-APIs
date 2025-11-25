cd092b200ace28706edabb841eb2d4814e7373f1
Monika <monika.ragauskiene@roxit.nl>
2025-11-25 10:04:04 +0200

    Splitted ZGWApiServiceCollectionExtensions into separate files

---
+++ src/OneGround.ZGW.Common.Web/Extensions/ServiceCollection/ZGWApiExtensions/ConfigureForwardedHeadersServiceCollectionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OneGround.ZGW.Common.Web.Extensions.ServiceCollection.ZGWApiExtensions;
+
+public static class ConfigureForwardedHeadersServiceCollectionExtensions
+{
+    public static IServiceCollection ConfigureForwardedHeaders(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ForwardedHeadersOptions>(options =>
+        {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+
+            var knownNetworks = configuration.GetSection("ForwardedHeaders:KnownNetworks").Get<string[]>() ?? [];
+            if (knownNetworks.Length != 0)
+            {
+                foreach (var network in knownNetworks)
+                {
+                    var parts = network.Split('/');
+
+                    if (parts.Length == 2 && IPAddress.TryParse(parts[0], out var ipAddress) && int.TryParse(parts[1], out var prefixLength))
+                    {
+                        options.KnownNetworks.Add(new Microsoft.AspNetCore.HttpOverrides.IPNetwork(ipAddress, prefixLength));
+                    }
+                    else
+                    {
+                        throw new FormatException(
+                            $"Invalid network format in 'ForwardedHeaders:KnownNetworks': '{network}'. Expected format is '[IPAddress]/[PrefixLength]'."
+                        );
+                    }
+                }
+            }
+
+            var knownProxies = configuration.GetSection("ForwardedHeaders:KnownProxies").Get<string[]>() ?? [];
+            if (knownProxies.Length != 0)
+            {
+                foreach (var proxy in knownProxies)
+                {
+                    options.KnownProxies.Add(IPAddress.Parse(proxy));
+                }
+            }
+
+            var resolverForwardedHeader = configuration.GetValue("Application:ResolveForwardedHost", false);
+            if (resolverForwardedHeader)
+            {
+                options.ForwardedHeaders |= ForwardedHeaders.XForwardedHost;
+            }
+        });
+
+        return services;
+    }
+}
---
+++ src/OneGround.ZGW.Common.Web/Extensions/ServiceCollection/ZGWApiExtensions/MediatorServiceCollectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using OneGround.ZGW.Common.Web.Handlers;
+
+namespace OneGround.ZGW.Common.Web.Extensions.ServiceCollection.ZGWApiExtensions;
+
+public static class MediatorServiceCollectionExtensions
+{
+    public static IServiceCollection AddMediator(this IServiceCollection services, Assembly callingAssembly, ZGWApiServiceSettings apiServiceSettings)
+    {
+        services.AddMediatR(x =>
+        {
+            x.RegisterServicesFromAssemblies(callingAssembly);
+        });
+
+        if (apiServiceSettings.RegisterSharedAudittrailHandlers)
+        {
+            services.AddMediatR(x =>
+            {
+                x.RegisterServicesFromAssemblies(typeof(LogAuditTrailGetObjectListCommand).GetTypeInfo()
+                    .Assembly); // The shared command handlers for audittrail for some API's not all
+            });
+        }
+
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(HandlerLoggingBehavior<,>));
+
+        return services;
+    }
+}
