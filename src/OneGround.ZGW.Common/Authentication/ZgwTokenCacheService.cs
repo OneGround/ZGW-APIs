1681b2e375a5b92aad1f8851e5f30ac01a72ea45
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2025-11-26 08:30:26 +0100

    code review point

--- src/OneGround.ZGW.Common/Authentication/ZgwTokenCacheService.cs
+++ src/OneGround.ZGW.Common/Authentication/ZgwTokenCacheService.cs
@@ -38,6 +38,6 @@ public class ZgwTokenCacheService : IZgwTokenCacheService
             }
         );
 
-        return (tokenWithExpiration.AccessToken, tokenWithExpiration.expiration);
+        return tokenWithExpiration;
     }
 }
