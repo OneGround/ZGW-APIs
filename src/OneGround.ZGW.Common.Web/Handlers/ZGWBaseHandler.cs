1cb0972e63e552529761996d254253ca7cb1d7d1
Monika <monika.ragauskiene@roxit.nl>
2025-12-05 12:54:12 +0200

    FUND-2011 Removed AuthorizedRsins settings

--- src/OneGround.ZGW.Common.Web/Handlers/ZGWBaseHandler.cs
+++ src/OneGround.ZGW.Common.Web/Handlers/ZGWBaseHandler.cs
@@ -14,35 +14,21 @@ public abstract class ZGWBaseHandler
     protected readonly IAuthorizationContextAccessor AuthorizationContextAccessor;
     protected readonly string _rsin;
 
-    private readonly IEnumerable<string> _authorizedRsins;
-
     protected ZGWBaseHandler(IConfiguration configuration, IAuthorizationContextAccessor authorizationContextAccessor)
     {
         Configuration = configuration;
         AuthorizationContextAccessor = authorizationContextAccessor;
         _rsin = authorizationContextAccessor.AuthorizationContext.Authorization.Rsin;
-
-        _authorizedRsins = Configuration.GetSection("Application:AuthorizedRsins").Get<IEnumerable<string>>() ?? [];
     }
 
     protected Expression<Func<T, bool>> GetRsinFilterPredicate<T>()
         where T : OwnedEntity
     {
-        if (_authorizedRsins.Contains(_rsin))
-        {
-            return z => true;
-        }
-
         return z => z.Owner == _rsin;
     }
 
     protected Expression<Func<T, bool>> GetRsinFilterPredicate<T>(Expression<Func<T, bool>> predicate)
     {
-        if (_authorizedRsins.Contains(_rsin))
-        {
-            return z => true;
-        }
-
         return predicate;
     }
 }
