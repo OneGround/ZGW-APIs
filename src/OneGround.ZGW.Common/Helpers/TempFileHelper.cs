e2938732d221aa1ef515069e6694f21eddc37442
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2025-12-10 13:03:57 +0100

    FileSystem-document-provider setting SecurityCheckOnFilePath added

--- src/OneGround.ZGW.Common/Helpers/TempFileHelper.cs
+++ src/OneGround.ZGW.Common/Helpers/TempFileHelper.cs
@@ -1,11 +1,11 @@
-﻿using System.IO;
+using System.IO;
 using System.Security;
 
 namespace OneGround.ZGW.Common.Helpers;
 
 public static class TempFileHelper
 {
-    public static string GetValidatedPath(string fullFileName)
+    public static string GetValidatedPath(string fullFileName, bool trustedPath = false)
     {
         if (fullFileName == null)
             return null;
@@ -13,14 +13,16 @@ public static class TempFileHelper
         if (fullFileName.Contains(".."))
             throw new SecurityException("Full file name contains not allowed characters.");
 
-        var directory = Path.GetDirectoryName(fullFileName);
+        if (!trustedPath)
+        {
+            var directory = Path.GetDirectoryName(fullFileName);
 
-        if (directory == null)
-            return null;
+            if (directory == null)
+                return null;
 
-        if (!directory.StartsWith(Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar)))
-            throw new SecurityException("Temporary file does not contain the temporary folder which should be.");
-
+            if (!directory.StartsWith(Path.GetTempPath().TrimEnd(Path.DirectorySeparatorChar)))
+                throw new SecurityException("Temporary file does not contain the temporary folder which should be.");
+        }
         return fullFileName;
     }
 }
--- src/OneGround.ZGW.Documenten.Services.FileSystem/FileSystemDocumentServiceSettings.cs
+++ src/OneGround.ZGW.Documenten.Services.FileSystem/FileSystemDocumentServiceSettings.cs
@@ -3,4 +3,5 @@ namespace OneGround.ZGW.Documenten.Services.FileSystem;
 public class FileSystemDocumentServiceSettings
 {
     public string DocumentRootPath { get; set; }
+    public bool SecurityCheckOnFilePath { get; set; } = true;
 }
