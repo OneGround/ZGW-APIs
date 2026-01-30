ed1b3e3ab4adc1cd4f17bed5701cfc5015b2c8b5
Giedrius Grabauskas <43740166+Grabauskas@users.noreply.github.com>
2026-01-30 11:52:44 +0200

    Issue: Structurally dead code

--- src/OneGround.ZGW.Zaken.Web/Services/BronDate/ZaakObjectBronDate.cs
+++ src/OneGround.ZGW.Zaken.Web/Services/BronDate/ZaakObjectBronDate.cs
@@ -31,7 +31,7 @@ public class ZaakObjectBronDate : IBronDateService
         _context = context;
     }
 
-    public async Task<DateOnly?> GetAsync(Zaak zaak, List<ArchiveValidationError> errors, CancellationToken cancellationToken)
+    public Task<DateOnly?> GetAsync(Zaak zaak, List<ArchiveValidationError> errors, CancellationToken cancellationToken)
     {
         // TODO: Must be worked out how to deal with this situation. Log and fallback on archiefstatus => ArchiefStatus.gearchiveerd_procestermijn_onbekend
         errors.Add(
@@ -41,87 +41,87 @@ public class ZaakObjectBronDate : IBronDateService
                 "De brondatum kan (nog) niet juist bepaald worden indien Afleidingswijze is zaakobject."
             )
         );
-        return null;
+        return Task.FromResult<DateOnly?>(null);
         // ----
 
-        if (!_objectType.HasValue)
-        {
-            errors.Add(
-                new ArchiveValidationError(
-                    string.Empty,
-                    ErrorCode.ArchiefActieDatumError,
-                    $"Geen objecttype aanwezig om het zaakobject te achterhalen voor het bepalen van de brondatum."
-                )
-            );
-            return null;
-        }
-
-        if (string.IsNullOrEmpty(_datumKenmerk))
-        {
-            errors.Add(
-                new ArchiveValidationError(
-                    string.Empty,
-                    ErrorCode.ArchiefActieDatumError,
-                    $"Geen datumkenmerk aanwezig om het attribuut van het zaakobject te achterhalen voor het bepalen van de brondatum."
-                )
-            );
-            return null;
-        }
-
-        var zaakObjecten = await _context
-            .ZaakObjecten.AsNoTracking()
-            .Include(z => z.WozWaardeObject)
-            .Include(z => z.Overige)
-            .Where(z => z.ZaakId == zaak.Id)
-            .Where(z => z.ObjectType == _objectType.Value)
-            .ToListAsync(cancellationToken);
-
-        var maxDate = DateOnly.MinValue;
-        foreach (var zaakObject in zaakObjecten)
-        {
-            var value = await GetObject(zaakObject);
-            if (value == null || !value.TryGetValue(_datumKenmerk, out var datumKenmerk))
-            {
-                errors.Add(
-                    new ArchiveValidationError(
-                        string.Empty,
-                        ErrorCode.ArchiefActieDatumError,
-                        $"{_datumKenmerk} geen geldig attribuut voor ZaakObject van type {zaakObject.ObjectType}"
-                    )
-                );
-                return null;
-            }
-            var dateValue = datumKenmerk as string;
-            if (!DateOnly.TryParse(dateValue, out var date))
-            {
-                errors.Add(
-                    new ArchiveValidationError(
-                        string.Empty,
-                        ErrorCode.ArchiefActieDatumError,
-                        $"Geen geldige datumwaarde in attribuut \"{_datumKenmerk}\": {dateValue}'"
-                    )
-                );
-                return null;
-            }
-            if (date > maxDate)
-            {
-                maxDate = date;
-            }
-        }
-
-        if (maxDate == DateOnly.MinValue)
-        {
-            errors.Add(
-                new ArchiveValidationError(
-                    string.Empty,
-                    ErrorCode.ArchiefActieDatumError,
-                    $"Geen attribuut gevonden die overeenkomt met het datumkenmerk \"{_datumKenmerk}\" voor het bepalen van de brondatum."
-                )
-            );
-            return null;
-        }
-
-        return maxDate;
+        // if (!_objectType.HasValue)
+        // {
+        //     errors.Add(
+        //         new ArchiveValidationError(
+        //             string.Empty,
+        //             ErrorCode.ArchiefActieDatumError,
+        //             $"Geen objecttype aanwezig om het zaakobject te achterhalen voor het bepalen van de brondatum."
+        //         )
+        //     );
+        //     return null;
+        // }
+        //
+        // if (string.IsNullOrEmpty(_datumKenmerk))
+        // {
+        //     errors.Add(
+        //         new ArchiveValidationError(
+        //             string.Empty,
+        //             ErrorCode.ArchiefActieDatumError,
+        //             $"Geen datumkenmerk aanwezig om het attribuut van het zaakobject te achterhalen voor het bepalen van de brondatum."
+        //         )
+        //     );
+        //     return null;
+        // }
+        //
+        // var zaakObjecten = await _context
+        //     .ZaakObjecten.AsNoTracking()
+        //     .Include(z => z.WozWaardeObject)
+        //     .Include(z => z.Overige)
+        //     .Where(z => z.ZaakId == zaak.Id)
+        //     .Where(z => z.ObjectType == _objectType.Value)
+        //     .ToListAsync(cancellationToken);
+        //
+        // var maxDate = DateOnly.MinValue;
+        // foreach (var zaakObject in zaakObjecten)
+        // {
+        //     var value = await GetObject(zaakObject);
+        //     if (value == null || !value.TryGetValue(_datumKenmerk, out var datumKenmerk))
+        //     {
+        //         errors.Add(
+        //             new ArchiveValidationError(
+        //                 string.Empty,
+        //                 ErrorCode.ArchiefActieDatumError,
+        //                 $"{_datumKenmerk} geen geldig attribuut voor ZaakObject van type {zaakObject.ObjectType}"
+        //             )
+        //         );
+        //         return null;
+        //     }
+        //     var dateValue = datumKenmerk as string;
+        //     if (!DateOnly.TryParse(dateValue, out var date))
+        //     {
+        //         errors.Add(
+        //             new ArchiveValidationError(
+        //                 string.Empty,
+        //                 ErrorCode.ArchiefActieDatumError,
+        //                 $"Geen geldige datumwaarde in attribuut \"{_datumKenmerk}\": {dateValue}'"
+        //             )
+        //         );
+        //         return null;
+        //     }
+        //     if (date > maxDate)
+        //     {
+        //         maxDate = date;
+        //     }
+        // }
+        //
+        // if (maxDate == DateOnly.MinValue)
+        // {
+        //     errors.Add(
+        //         new ArchiveValidationError(
+        //             string.Empty,
+        //             ErrorCode.ArchiefActieDatumError,
+        //             $"Geen attribuut gevonden die overeenkomt met het datumkenmerk \"{_datumKenmerk}\" voor het bepalen van de brondatum."
+        //         )
+        //     );
+        //     return null;
+        // }
+        //
+        // return maxDate;
     }
 
     private async Task<IDictionary<string, object>> GetObject(ZaakObject zaakObject)
