59c36daed975bda0f067b0aeacbf7eeacb32e8a6
Monika <monika.ragauskiene@roxit.nl>
2026-02-20 16:17:44 +0200

    FUND-2357 Added validation and migration

---
+++ src/OneGround.ZGW.Zaken.DataModel/Migrations/20260220130816_update_zaakobjecten_overigen_column_overigeData_jsonb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Zaken.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_zaakobjecten_overigen_column_overigeData_jsonb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // PostgreSQL requires explicit conversion from text to jsonb
+            migrationBuilder.Sql(
+                @"ALTER TABLE zaakobjecten_overigen
+                  ALTER COLUMN overigedata TYPE jsonb USING overigedata::jsonb;"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revert jsonb back to text
+            migrationBuilder.Sql(
+                @"ALTER TABLE zaakobjecten_overigen
+                  ALTER COLUMN overigedata TYPE text USING overigedata::text;"
+            );
+        }
+    }
+}
