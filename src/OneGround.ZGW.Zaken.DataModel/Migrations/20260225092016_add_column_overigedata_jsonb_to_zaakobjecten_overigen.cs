e799ebe5c77326bde690b6d9a40744cca8dca6c9
Monika <monika.ragauskiene@roxit.nl>
2026-02-25 09:49:00 +0200

    FUND-2357 ZRC added new migration to create new column

---
+++ src/OneGround.ZGW.Zaken.DataModel/Migrations/20260225092016_add_column_overigedata_jsonb_to_zaakobjecten_overigen.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Zaken.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_column_overigedata_jsonb_to_zaakobjecten_overigen : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(name: "overigedata_jsonb", table: "zaakobjecten_overigen", type: "jsonb", nullable: true);
+
+            migrationBuilder.Sql(
+                "CREATE INDEX CONCURRENTLY idx_zaakobjecten_overigen_backfill " + "ON zaakobjecten_overigen (id) WHERE overigedata_jsonb IS NULL;",
+                suppressTransaction: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP INDEX CONCURRENTLY IF EXISTS idx_zaakobjecten_overigen_backfill;", suppressTransaction: true);
+
+            migrationBuilder.DropColumn(name: "overigedata_jsonb", table: "zaakobjecten_overigen");
+        }
+    }
+}
