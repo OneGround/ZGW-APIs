6438d83f412dababc66b22c0b15ef93a30909538
Monika Ragauskienė <monika.ragauskiene@roxit.nl>
2026-05-04 12:00:24 +0300

    Removed comments

--- src/Tests/OneGround.ZGW.Zaken.WebApi.UnitTests/MappingTests/RequestToDomainProfileTests.cs
+++ src/Tests/OneGround.ZGW.Zaken.WebApi.UnitTests/MappingTests/RequestToDomainProfileTests.cs
@@ -419,8 +419,8 @@ public class RequestToDomainProfileTests
         var result = _mapper.Map<NatuurlijkPersoonZaakRol>(value);
 
         Assert.Equal(value.InpBsn, result.InpBsn);
-        Assert.Null(result.InpBsnHash); // InpBsnHash is set explicitly by the command handler, not by AutoMapper
-        Assert.Null(result.InpBsnHashKeyVersion); // InpBsnHashKeyVersion is set explicitly by the command handler
+        Assert.Null(result.InpBsnHash);
+        Assert.Null(result.InpBsnHashKeyVersion);
         Assert.Equal(value.InpBsn, result.InpBsnEncrypted);
         Assert.Equal(value.AnpIdentificatie, result.AnpIdentificatie);
         Assert.Equal(value.InpANummer, result.InpANummer);
