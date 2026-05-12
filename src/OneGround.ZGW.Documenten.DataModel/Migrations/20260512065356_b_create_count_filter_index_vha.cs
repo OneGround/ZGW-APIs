0aded672d505ce980f44609b34d56127cd2beaed
Giedrius Grabauskas <43740166+Grabauskas@users.noreply.github.com>
2026-05-12 10:01:37 +0300

    Created t3b_IX_eio_owner_iot_latest_vha index

---
+++ src/OneGround.ZGW.Documenten.DataModel/Migrations/20260512065356_b_create_count_filter_index_vha.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Documenten.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class b_create_count_filter_index_vha : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"
+CREATE INDEX CONCURRENTLY IF NOT EXISTS ""t3b_IX_eio_owner_iot_latest_vha""
+    ON public.enkelvoudiginformatieobjecten USING btree
+    (owner COLLATE pg_catalog.""default"" ASC NULLS LAST,
+     informatieobjecttype COLLATE pg_catalog.""default"" ASC NULLS LAST,
+     latest_vertrouwelijkheidaanduiding ASC NULLS LAST)
+    TABLESPACE pg_default;
+",
+                suppressTransaction: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP INDEX CONCURRENTLY IF EXISTS public.""t3b_IX_eio_owner_iot_latest_vha"";", suppressTransaction: true);
+        }
+    }
+}
