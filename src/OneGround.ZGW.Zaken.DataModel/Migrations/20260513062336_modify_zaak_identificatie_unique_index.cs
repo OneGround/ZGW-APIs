c5cca5de463c1f8c0dbe954ccdc58bbf04f230c8
heuvea <66989902+heuvea@users.noreply.github.com>
2026-06-16 10:42:27 +0200

    FUND-2548 ZRC and BRC The combination of identificatie+owner should be unique (#182)
    * fix ZRC and BRC combination of identificatie owner should be unique
    
    * co-pilot review fixes
    
    * current date+time when applying BRC migration
    
    * removed the filter HasFilter
    
    * CreatedConcurrently added at drop index
    
    * code review fix

---
+++ src/OneGround.ZGW.Zaken.DataModel/Migrations/20260513062336_modify_zaak_identificatie_unique_index.cs
@@ -0,0 +1,48 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Zaken.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class modify_zaak_identificatie_unique_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_zaken_bronorganisatie_identificatie", table: "zaken").Annotation("Npgsql:CreatedConcurrently", true);
+
+            migrationBuilder.DropIndex(name: "IX_zaken_owner_identificatie", table: "zaken").Annotation("Npgsql:CreatedConcurrently", true);
+
+            migrationBuilder
+                .CreateIndex(
+                    name: "IX_zaken_owner_identificatie",
+                    table: "zaken",
+                    columns: new[] { "owner", "identificatie" },
+                    unique: true,
+                    filter: $"creationtime > '{DateTime.UtcNow:yyyy-MM-ddTHH:mm:ssZ}'"
+                )
+                .Annotation("Npgsql:CreatedConcurrently", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_zaken_owner_identificatie", table: "zaken").Annotation("Npgsql:CreatedConcurrently", true);
+
+            migrationBuilder
+                .CreateIndex(
+                    name: "IX_zaken_bronorganisatie_identificatie",
+                    table: "zaken",
+                    columns: new[] { "bronorganisatie", "identificatie" },
+                    unique: true
+                )
+                .Annotation("Npgsql:CreatedConcurrently", true);
+
+            migrationBuilder
+                .CreateIndex(name: "IX_zaken_owner_identificatie", table: "zaken", columns: new[] { "owner", "identificatie" })
+                .Annotation("Npgsql:CreatedConcurrently", true);
+        }
+    }
+}
