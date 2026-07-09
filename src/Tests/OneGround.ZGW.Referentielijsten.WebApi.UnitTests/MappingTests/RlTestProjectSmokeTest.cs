ecf3d9bc230ed0fb227f017aace72932cfb21f6e
Monika Ragauskienė <monika.ragauskiene@roxit.nl>
2026-07-09 16:36:11 +0300

    test(rl): scaffold Referentielijsten WebApi unit-test project

---
+++ src/Tests/OneGround.ZGW.Referentielijsten.WebApi.UnitTests/MappingTests/RlTestProjectSmokeTest.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace OneGround.ZGW.Referentielijsten.WebApi.UnitTests.MappingTests;
+
+public class RlTestProjectSmokeTest
+{
+    [Fact]
+    public void Test_project_builds_and_runs()
+    {
+        Assert.True(true);
+    }
+}
