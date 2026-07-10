9685f45eee40fa7c2b28f332dbdd735701a2d668
Monika Ragauskienė <monika.ragauskiene@roxit.nl>
2026-07-10 15:46:15 +0300

    feat(ztc): add v1 DomainToResponseRegister (Mapster)
    Ports the v1 Catalogi DomainToResponseProfile AutoMapper source to Mapster,
    introducing two new patterns for this migration: IMappingAction.AfterMap ->
    .AfterMapping lambdas resolving IEntityUriService via MapContext, and folding
    AutoMapper's PreCondition null-guards directly into the projection lambda
    (11 occurrences). Retargets DomainToResponseProfileTests to the DI-wired
    ServiceMapper pattern and adds a dedicated fact for the GerelateerdeZaakTypen
    AfterMapping logic, verified via deliberate breakage.

---
+++ src/OneGround.ZGW.Catalogi.Web/MappingProfiles/v1/DomainToResponseRegister.cs
@@ -0,0 +1,253 @@
+using System.Linq;
+using Mapster;
+using MapsterMapper;
+using OneGround.ZGW.Catalogi.Contracts.v1;
+using OneGround.ZGW.Catalogi.Contracts.v1.Requests;
+using OneGround.ZGW.Catalogi.Contracts.v1.Responses;
+using OneGround.ZGW.Catalogi.DataModel;
+using OneGround.ZGW.Common.Helpers;
+using OneGround.ZGW.Common.Web.Mapping.Mapster;
+using OneGround.ZGW.Common.Web.Services.UriServices;
+
+namespace OneGround.ZGW.Catalogi.Web.MappingProfiles.v1;
+
+public class DomainToResponseRegister : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config
+            .NewConfig<ZaakType, ZaakTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(dest => dest.VersieDatum, src => ProfileHelper.StringDateFromDate(src.VersieDatum))
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus))
+            .Map(dest => dest.StatusTypen, src => MapsterUrlResolver.ResolveUrls(src.StatusTypen))
+            .Map(dest => dest.RolTypen, src => MapsterUrlResolver.ResolveUrls(src.RolTypen))
+            .Map(dest => dest.ResultaatTypen, src => MapsterUrlResolver.ResolveUrls(src.ResultaatTypen))
+            .Map(dest => dest.Eigenschappen, src => MapsterUrlResolver.ResolveUrls(src.Eigenschappen))
+            .Map(dest => dest.VerlengingsTermijn, src => ProfileHelper.Fix0Period(src.VerlengingsTermijn))
+            .Map(dest => dest.Servicenorm, src => ProfileHelper.Fix0Period(src.Servicenorm))
+            .Map(dest => dest.Doorlooptijd, src => ProfileHelper.Fix0Period(src.Doorlooptijd))
+            .Ignore(dest => dest.GerelateerdeZaakTypen)
+            .Map(
+                dest => dest.InformatieObjectTypen,
+                src =>
+                    src.ZaakTypeInformatieObjectTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(
+                            src.ZaakTypeInformatieObjectTypen.Where(i => i.InformatieObjectType != null).Select(s => s.InformatieObjectType)
+                        )
+            )
+            .Map(
+                dest => dest.DeelZaakTypen,
+                src =>
+                    src.ZaakTypeDeelZaakTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.ZaakTypeDeelZaakTypen.Where(z => z.DeelZaakType != null).Select(s => s.DeelZaakType))
+            )
+            .Map(
+                dest => dest.BesluitTypen,
+                src =>
+                    src.ZaakTypeBesluitTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.ZaakTypeBesluitTypen.Where(b => b.BesluitType != null).Select(b => b.BesluitType))
+            )
+            .AfterMapping(
+                (src, dest) =>
+                {
+                    var uriService = MapContext.Current.GetService<IEntityUriService>();
+                    dest.GerelateerdeZaakTypen = src
+                        .ZaakTypeGerelateerdeZaakTypen.Where(z => z.GerelateerdeZaakType != null)
+                        .Select(z => new GerelateerdeZaaktypeDto
+                        {
+                            AardRelatie = z.AardRelatie.ToString(),
+                            Toelichting = z.Toelichting,
+                            ZaakType = uriService.GetUri(z.GerelateerdeZaakType),
+                        })
+                        .ToList();
+                }
+            );
+
+        config.NewConfig<ReferentieProces, ReferentieProcesDto>();
+
+        // Note: This map is used to merge an existing ZAAKTYPE with the PATCH operation
+        config
+            .NewConfig<ZaakType, ZaakTypeRequestDto>()
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus))
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(dest => dest.VersieDatum, src => ProfileHelper.StringDateFromDate(src.VersieDatum))
+            .Ignore(dest => dest.GerelateerdeZaakTypen)
+            .Map(
+                dest => dest.DeelZaakTypen,
+                src =>
+                    src.ZaakTypeDeelZaakTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.ZaakTypeDeelZaakTypen.Where(z => z.DeelZaakType != null).Select(s => s.DeelZaakType))
+            )
+            .Map(
+                dest => dest.BesluitTypen,
+                src =>
+                    src.ZaakTypeBesluitTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.ZaakTypeBesluitTypen.Where(b => b.BesluitType != null).Select(b => b.BesluitType))
+            )
+            .AfterMapping(
+                (src, dest) =>
+                {
+                    var uriService = MapContext.Current.GetService<IEntityUriService>();
+                    dest.GerelateerdeZaakTypen = src
+                        .ZaakTypeGerelateerdeZaakTypen.Where(z => z.GerelateerdeZaakType != null)
+                        .Select(z => new GerelateerdeZaaktypeDto
+                        {
+                            AardRelatie = z.AardRelatie.ToString(),
+                            Toelichting = z.Toelichting,
+                            ZaakType = uriService.GetUri(z.GerelateerdeZaakType),
+                        })
+                        .ToList();
+                }
+            );
+
+        config
+            .NewConfig<StatusType, StatusTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType))
+            .Map(dest => dest.OmschrijvingGeneriek, src => ProfileHelper.EmptyWhenNull(src.OmschrijvingGeneriek))
+            .Map(dest => dest.StatusTekst, src => ProfileHelper.EmptyWhenNull(src.StatusTekst));
+
+        // Note: This map is used to merge an existing STATUSTYPE with the PATCH operation
+        config.NewConfig<StatusType, StatusTypeRequestDto>().Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        config
+            .NewConfig<RolType, RolTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        // Note: This map is used to merge an existing RolType with the PATCH operation
+        config.NewConfig<RolType, RolTypeRequestDto>().Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        config
+            .NewConfig<ZaakTypeInformatieObjectType, ZaakTypeInformatieObjectTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType))
+            .Map(dest => dest.StatusType, src => MapsterUrlResolver.ResolveUrl(src.StatusType))
+            .Map(dest => dest.InformatieObjectType, src => MapsterUrlResolver.ResolveUrl(src.InformatieObjectType));
+
+        // Note: This map is used to merge an existing ZaakTypeInformatieObjectTypen with the PATCH operation
+        config
+            .NewConfig<ZaakTypeInformatieObjectType, ZaakTypeInformatieObjectTypeRequestDto>()
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType))
+            .Map(dest => dest.StatusType, src => MapsterUrlResolver.ResolveUrl(src.StatusType))
+            .Map(dest => dest.InformatieObjectType, src => MapsterUrlResolver.ResolveUrl(src.InformatieObjectType));
+
+        config
+            .NewConfig<ResultaatType, ResultaatTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType))
+            .Map(dest => dest.ArchiefActieTermijn, src => ProfileHelper.Fix0Period(src.ArchiefActieTermijn));
+
+        // Note: for PATCH operation
+        config.NewConfig<ResultaatType, ResultaatTypeRequestDto>().Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        config
+            .NewConfig<BronDatumArchiefProcedure, BronDatumArchiefProcedureDto>()
+            .Map(dest => dest.ProcesTermijn, src => ProfileHelper.Fix0Period(src.ProcesTermijn));
+
+        config
+            .NewConfig<Catalogus, CatalogusResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakTypen, src => MapsterUrlResolver.ResolveUrls(src.ZaakTypes))
+            .Map(dest => dest.BesluitTypen, src => MapsterUrlResolver.ResolveUrls(src.BesluitTypes))
+            .Map(dest => dest.InformatieObjectTypen, src => MapsterUrlResolver.ResolveUrls(src.InformatieObjectTypes));
+
+        config
+            .NewConfig<InformatieObjectType, InformatieObjectTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus))
+            .Map(
+                dest => dest.ZaakTypen,
+                src =>
+                    src.InformatieObjectTypeZaakTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.InformatieObjectTypeZaakTypen.Where(z => z.ZaakType != null).Select(b => b.ZaakType))
+            )
+            .Map(
+                dest => dest.BesluitTypen,
+                src =>
+                    src.InformatieObjectTypeBesluitTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(
+                            src.InformatieObjectTypeBesluitTypen.Where(z => z.BesluitType != null).Select(b => b.BesluitType)
+                        )
+            );
+
+        // Note: for PATCH operation
+        config
+            .NewConfig<InformatieObjectType, InformatieObjectTypeRequestDto>()
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus))
+            .Ignore(dest => dest.ZaakTypen)
+            .Ignore(dest => dest.BesluitTypen);
+
+        config.NewConfig<EigenschapSpecificatie, EigenschapSpecificatieDto>();
+        config
+            .NewConfig<Eigenschap, EigenschapResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        // Note: for PATCH operation
+        config.NewConfig<Eigenschap, EigenschapRequestDto>().Map(dest => dest.ZaakType, src => MapsterUrlResolver.ResolveUrl(src.ZaakType));
+
+        config
+            .NewConfig<BesluitType, BesluitTypeResponseDto>()
+            .Map(dest => dest.Url, src => MapsterUrlResolver.ResolveUrl(src))
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(
+                dest => dest.ZaakTypen,
+                src =>
+                    src.BesluitTypeZaakTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.BesluitTypeZaakTypen.Where(b => b.ZaakType != null).Select(b => b.ZaakType))
+            )
+            .Map(
+                dest => dest.InformatieObjectTypen,
+                src =>
+                    src.BesluitTypeInformatieObjectTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(
+                            src.BesluitTypeInformatieObjectTypen.Where(b => b.InformatieObjectType != null).Select(b => b.InformatieObjectType)
+                        )
+            )
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus))
+            .Map(dest => dest.ReactieTermijn, src => ProfileHelper.Fix0Period(src.ReactieTermijn))
+            .Map(dest => dest.PublicatieTermijn, src => ProfileHelper.Fix0Period(src.PublicatieTermijn));
+
+        // Note: for PATCH operation
+        config
+            .NewConfig<BesluitType, BesluitTypeRequestDto>()
+            .Map(dest => dest.BeginGeldigheid, src => ProfileHelper.StringDateFromDate(src.BeginGeldigheid))
+            .Map(dest => dest.EindeGeldigheid, src => ProfileHelper.StringDateFromDate(src.EindeGeldigheid))
+            .Map(
+                dest => dest.ZaakTypen,
+                src =>
+                    src.BesluitTypeZaakTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(src.BesluitTypeZaakTypen.Where(b => b.ZaakType != null).Select(b => b.ZaakType))
+            )
+            .Map(
+                dest => dest.InformatieObjectTypen,
+                src =>
+                    src.BesluitTypeInformatieObjectTypen == null
+                        ? null
+                        : MapsterUrlResolver.ResolveUrls(
+                            src.BesluitTypeInformatieObjectTypen.Where(b => b.InformatieObjectType != null).Select(b => b.InformatieObjectType)
+                        )
+            )
+            .Map(dest => dest.Catalogus, src => MapsterUrlResolver.ResolveUrl(src.Catalogus));
+    }
+}
