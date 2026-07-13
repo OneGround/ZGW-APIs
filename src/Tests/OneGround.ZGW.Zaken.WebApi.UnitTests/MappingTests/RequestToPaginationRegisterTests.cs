cae18db661c7e57622e5f5fa2be1d01236e35190
Monika Ragauskienė <monika.ragauskiene@roxit.nl>
2026-07-13 14:52:06 +0300

    feat(zrc): add RequestToPaginationRegister (Mapster)

---
+++ src/Tests/OneGround.ZGW.Zaken.WebApi.UnitTests/MappingTests/RequestToPaginationRegisterTests.cs
@@ -0,0 +1,32 @@
+using Mapster;
+using MapsterMapper;
+using OneGround.ZGW.Common.Contracts.v1;
+using OneGround.ZGW.Common.Web.Models;
+using OneGround.ZGW.Zaken.Web.MappingProfiles;
+using Xunit;
+
+namespace OneGround.ZGW.Zaken.WebApi.UnitTests.MappingTests;
+
+public class RequestToPaginationRegisterTests
+{
+    private readonly IMapper _mapper;
+
+    public RequestToPaginationRegisterTests()
+    {
+        var config = new TypeAdapterConfig();
+        new RequestToPaginationRegister().Register(config);
+        config.Compile();
+        _mapper = new Mapper(config);
+    }
+
+    [Fact]
+    public void PaginationQuery_Maps_To_PaginationFilter()
+    {
+        var query = new PaginationQuery(page: 3, pageSize: 42);
+
+        var result = _mapper.Map<PaginationFilter>(query);
+
+        Assert.Equal(3, result.Page);
+        Assert.Equal(42, result.Size);
+    }
+}
