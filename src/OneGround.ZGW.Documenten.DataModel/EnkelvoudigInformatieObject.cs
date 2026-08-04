403534b5ef15b29df9f0230e9769e8cb8c86abb6
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2026-08-04 11:23:17 +0200

    created the new fields for v1.7

--- src/OneGround.ZGW.Documenten.DataModel/EnkelvoudigInformatieObject.cs
+++ src/OneGround.ZGW.Documenten.DataModel/EnkelvoudigInformatieObject.cs
@@ -43,6 +43,12 @@ public class EnkelvoudigInformatieObject : OwnedEntity, IAuditableEntity, IUrlEn
     [Column("locked")]
     public bool Locked { get; set; }
 
+    [Column("isgereedvoorpublicatie")]
+    public bool? IsGereedVoorPublicatie { get; set; }
+
+    [Column("tonenaaninitiator")]
+    public bool TonenAanInitiator { get; set; }
+
     [Column("lock")]
     public string Lock { get; set; }
 
---
+++ src/OneGround.ZGW.Documenten.DataModel/Migrations/20260804090646_add_columns_for_v1_7.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OneGround.ZGW.Documenten.DataModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_columns_for_v1_7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(name: "isgereedvoorpublicatie", table: "enkelvoudiginformatieobjecten", type: "boolean", nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "tonenaaninitiator",
+                table: "enkelvoudiginformatieobjecten",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "isgereedvoorpublicatie", table: "enkelvoudiginformatieobjecten");
+
+            migrationBuilder.DropColumn(name: "tonenaaninitiator", table: "enkelvoudiginformatieobjecten");
+        }
+    }
+}
