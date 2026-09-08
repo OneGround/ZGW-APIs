8be326e786783a9ae37d84e660cbb5ab191e9104
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2026-09-08 16:18:16 +0200

    Remove unused IExpandable/_expand from InformatieObjectTypeResponseDto (v1.3)

--- src/OneGround.ZGW.Catalogi.Contracts/v1/3/Responses/InformatieObjectTypeResponseDto.cs
+++ src/OneGround.ZGW.Catalogi.Contracts/v1/3/Responses/InformatieObjectTypeResponseDto.cs
@@ -1,17 +1,12 @@
-using System.Collections.Generic;
 using Newtonsoft.Json;
-using OneGround.ZGW.Common.Contracts;
 
 namespace OneGround.ZGW.Catalogi.Contracts.v1._3.Responses;
 
-public class InformatieObjectTypeResponseDto : InformatieObjectTypeDto, IExpandable
+public class InformatieObjectTypeResponseDto : InformatieObjectTypeDto
 {
     [JsonProperty("url", Order = 1)]
     public string Url { get; set; }
 
     [JsonProperty("concept", Order = 10)]
     public bool Concept { get; set; }
-
-    [JsonProperty("_expand", NullValueHandling = NullValueHandling.Ignore, Order = ExpandConstants.OrderLast)]
-    public Dictionary<string, object> Expand { get; set; }
 }
