7a0a62414f7fcb1da26800a80d028c734353bd8c
Aat van den Heuvel <aat.vandenheuvel@roxit.nl>
2026-09-15 08:24:40 +0200

    added validation expand/field (one otf those)

--- src/OneGround.ZGW.Documenten.Web/Validators/v1/7/EnkelvoudigInformatieObjectSearchRequestValidator.cs
+++ src/OneGround.ZGW.Documenten.Web/Validators/v1/7/EnkelvoudigInformatieObjectSearchRequestValidator.cs
@@ -1,5 +1,6 @@
 using FluentValidation;
 using Microsoft.Extensions.Configuration;
+using OneGround.ZGW.Common.Contracts.v1;
 using OneGround.ZGW.Common.Web.Validations;
 using OneGround.ZGW.Documenten.Contracts.v1._7.Requests;
 
@@ -9,6 +10,25 @@ public class EnkelvoudigInformatieObjectSearchRequestValidator : ZGWValidator<En
 {
     public EnkelvoudigInformatieObjectSearchRequestValidator(IConfiguration configuration)
     {
+        CascadeRuleFor(p => p)
+            .Custom(
+                (request, context) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(request.Expand) && request.Fields is not null)
+                    {
+                        context.AddFailure(
+                            new FluentValidation.Results.ValidationFailure(
+                                "fields",
+                                "Je mag niet zowel 'expand' als 'fields' opgeven in dezelfde aanvraag."
+                            )
+                            {
+                                ErrorCode = ErrorCode.Invalid,
+                            }
+                        );
+                    }
+                }
+            );
+
         CascadeRuleForEach(z => z.Uuid_In).IsGuid();
 
         CascadeRuleFor(p => p.Bronorganisatie).IsRsin(required: false);
